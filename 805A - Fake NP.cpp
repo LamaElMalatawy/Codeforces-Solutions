b462342b4829739415ddad4d763e3159b2b2c6df
LamaElMalatawy <74035870+LamaElMalatawy@users.noreply.github.com>
2021-05-12 01:15:37 +0200

    Add files via upload

---
+++ 805A - Fake NP.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define IO ios_base::sync_with_stdio(0),cin.tie(NULL),cout.tie(NULL)
+#define ll long long
+using namespace std;
+
+int main() {
+
+    IO;
+
+    ll l,r;
+    cin>>l>>r;
+    if (l==r )
+        cout<<r;
+    else
+        cout<<2;
+    return 0;
+}
