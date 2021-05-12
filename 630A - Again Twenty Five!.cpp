9e460653b7f30faaf76522bea7cd561ac0d048a3
LamaElMalatawy <74035870+LamaElMalatawy@users.noreply.github.com>
2021-05-12 15:00:33 +0200

    Create 630A - Again Twenty Five!.cpp

---
+++ 630A - Again Twenty Five!.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   long long n;
+   cin>>n;
+   if(n==0)
+    cout<<"01";
+   else if(n==1)
+    cout<<"05";
+   else
+   cout<<"25";
+       return 0;
+}
