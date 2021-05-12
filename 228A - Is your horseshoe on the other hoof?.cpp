786389f59e44dffa5f6d6262b152ec6b2bff9504
LamaElMalatawy <74035870+LamaElMalatawy@users.noreply.github.com>
2021-05-12 15:12:07 +0200

    Create 228A - Is your horseshoe on the other hoof?.cpp

---
+++ 228A - Is your horseshoe on the other hoof?.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    set<long long> s;
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+    s.insert(a);
+    s.insert(b);
+    s.insert(c);
+    s.insert(d);
+    cout<<4-s.size();
+    return 0;
+}
