d1e4ad8aa9fd561d4cfb419f26ee02b5e2114906
LamaElMalatawy <74035870+LamaElMalatawy@users.noreply.github.com>
2021-05-12 15:19:46 +0200

    Create 1438A - Specific Tastes of Andre.cpp

---
+++ 1438A - Specific Tastes of Andre.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t,n,ans;
+    cin>>t;
+    while(t--)
+    {
+       
+        cin>>n;
+       
+            for(int i=1; i<=n; i++)
+            {
+                cout<<2<<" ";
+                
+            }
+        
+        
+
+        cout<<endl;
+    }
+
+    return 0;
+}
